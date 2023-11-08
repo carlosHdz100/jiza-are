4c7e9ec42712088c32b38fe74a9b355b1bd70ae1
carlosHdz100 <chivacorazon44@gmail.com>
2023-11-08 08:53:58 -0600

    configurando modal

--- layouts/index.php
+++ layouts/index.php
@@ -40,7 +40,7 @@
             <a href="?view=categorias"><i class="fa fa-star"></i><span>Categorías</span></a>
             <a href="?view=favoritos" class="active-nav"><i class="fa fa-heart"></i><span>Favoritos</span></a>
             <a href="?view=carrito"><i class="fa fa-shop"></i><span>carrito</span></a>
-            <a href="#"><i class="fa fa-user"></i><span>Perfil</span></a>
+            <a href="#" data-menu="menu-settings"><i class="fa fa-user"></i><span>Perfil</span></a>
         </div>
 
 
@@ -48,7 +48,7 @@
         <?php include('config/views.php'); ?>
         <!-- End of Page Content-->
 
-        
+
 
         <!-- Menu Sidebar Sidebar Filters-->
         <div id="menu-filter" class="menu menu-box-left" data-menu-height="cover" data-menu-width="cover">
@@ -244,6 +244,42 @@
 
         </div>
 
+        <!-- All Menus, Action Sheets, Modals, Notifications, Toasts, Snackbars get Placed outside the <div class="page-content"> -->
+        <div id="menu-settings" class="menu menu-box-bottom menu-box-detached">
+            <div class="menu-title mt-0 pt-0">
+                <h1>Mi perfil</h1>
+            </div>
+            <div class="divider divider-margins mb-n2"></div>
+            <div class="content">
+                <div class="list-group list-custom-small">
+                    <a href="#" data-toggle-theme data-trigger-switch="switch-dark-mode" class="pb-2 ms-n1">
+                        <i class="fa font-12 fa-moon rounded-s bg-highlight color-white me-3"></i>
+                        <span>Dark Mode</span>
+                        <div class="custom-control scale-switch ios-switch">
+                            <input data-toggle-theme type="checkbox" class="ios-input" id="switch-dark-mode">
+                            <label class="custom-control-label" for="switch-dark-mode"></label>
+                        </div>
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
+                <div class="list-group list-custom-large">
+                    <a data-menu="menu-highlights" href="#">
+                        <i class="fa font-14 fa-tint bg-green-dark rounded-s"></i>
+                        <span>Page Highlight</span>
+                        <strong>16 Colors Highlights Included</strong>
+                        <span class="badge bg-highlight color-white">HOT</span>
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                    <a data-menu="menu-backgrounds" href="#" class="border-0">
+                        <i class="fa font-14 fa-cog bg-blue-dark rounded-s"></i>
+                        <span>Background Color</span>
+                        <strong>10 Page Gradients Included</strong>
+                        <span class="badge bg-highlight color-white">NEW</span>
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
 
 
 
