f223132710dfd85193ca6c8fee887fd10ec99442
carlosHdz100 <chivacorazon44@gmail.com>
2023-11-10 20:28:36 -0600

    Calendario

--- views/bolso.php
+++ views/bolso.php
@@ -669,7 +669,7 @@
         </div>
         <div class="d-flex justify-content-between">
             <h3>Precio: <strong>€200</strong></h3>
-            <button class="custom-btn btn-5"><span>Opciones de Fecha</span></button>
+            <button data-menu="menu-event-calendar" class="custom-btn btn-5"><span>Opciones de Fecha</span></button>
             <!-- <button type="button" class="btn btn-dark">Opciones de Fecha</button> -->
         </div>
 
@@ -683,6 +683,69 @@
     </div>
 </div>
 
-
+<div id="menu-event-calendar" class="menu menu-box-bottom menu-box-detached" data-menu-height="370" style="display: block; height: 370px; z-index:99999">
+<div class="calendar bg-theme m-0" style="max-width:100%!important;">
+<div class="cal-header">
+<h4 class="cal-title text-left font-700 bg-highlight color-white">Elige la fecha que necesites</h4>
+<h6 class="cal-title-right color-white close-menu"><i class="fa fa-times"></i></h6>
+</div>
+<div class="clearfix"></div>
+<div class="cal-days bg-highlight opacity-80 bottom-0">
+<a href="#">SUN</a>
+<a href="#">MON</a>
+<a href="#">TUE</a>
+<a href="#">WED</a>
+<a href="#">THU</a>
+<a href="#">FRI</a>
+<a href="#">SAT</a>
+<div class="clearfix"></div>
+</div>
+<div class="cal-dates cal-dates-border">
+<a href="#" class="cal-disabled">25</a>
+<a href="#" class="cal-disabled">26</a>
+<a href="#" class="cal-disabled">27</a>
+<a href="#" class="cal-disabled">28</a>
+<a href="#" class="cal-disabled">29</a>
+<a href="#" class="cal-disabled">30</a>
+<a href="#">1</a>
+<a href="#">2</a>
+<a href="#">3</a>
+<a href="#" class="cal-selected"><i class="fa fa-circle color-highlight"></i><span>5</span></a>
+<a href="#">5</a>
+<a href="#">6</a>
+<a href="#" class="cal-selected"><i class="fa fa-circle color-highlight"></i><span>7</span></a>
+<a href="#">8</a>
+<a href="#">9</a>
+<a href="#">10</a>
+<a href="#">11</a>
+<a href="#" class="cal-selected"><i class="fa fa-circle color-highlight"></i><span>12</span></a>
+<a href="#">13</a>
+<a href="#">14</a>
+<a href="#">15</a>
+<a href="#">16</a>
+<a href="#" class="cal-selected"><i class="fa fa-circle color-highlight"></i><span>5</span></a>
+<a href="#">18</a>
+<a href="#">19</a>
+<a href="#">20</a>
+<a href="#" class="cal-selected"><i class="fa fa-circle color-highlight"></i><span>5</span></a>
+<a href="#">22</a>
+<a href="#">23</a>
+<a href="#">24</a>
+<a href="#">25</a>
+<a href="#" class="cal-selected"><i class="fa fa-circle color-highlight"></i><span>26</span></a>
+<a href="#">27</a>
+<a href="#">28</a>
+<a href="#">29</a>
+<a href="#">30</a>
+<a href="#">31</a>
+<a href="#" class="cal-disabled">1</a>
+<a href="#" class="cal-disabled">2</a>
+<a href="#" class="cal-disabled">3</a>
+<a href="#" class="cal-disabled">4</a>
+<a href="#" class="cal-disabled">5</a>
+<div class="clearfix"></div>
+</div>
+</div>
+</div>
 <script src="assets/scripts/alerta-carrito.js"></script>
 <script src="assets/scripts/alerta-favorito.js"></script>
