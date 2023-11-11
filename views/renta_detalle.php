6e8d0fbe7a7f1af31d710cc98d0e2f8b897343d5
carlosHdz100 <chivacorazon44@gmail.com>
2023-11-10 18:10:47 -0600

    vista renta_datalle, en proceso

---
+++ views/renta_detalle.php
@@ -0,0 +1,30 @@
+<div class="page-content header-clear-medium">
+    <div class="card card-style">
+        <div class="content">
+            <h2>Articulos</h2>
+
+            <div class="d-flex mb-4">
+                <div>
+                    <img src="https://www.pronovias.com/media/catalog/product/l/a/lace_2236_b.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=747&width=560&canvas=560:747" class="rounded-m shadow-xl" width="80">
+                </div>
+                <div class="ms-3">
+                    <h5 class="font-600 opacity-50">Vestido de noche</h5>
+                    <h4 class="pt-1 color-highlight">$29.<sup>99</sup></h4>
+                    <span class="text-muted">(3 dias)</span>
+                </div>
+            </div>
+            <div class="d-flex">
+                <div>
+                    <img src="https://img.freepik.com/foto-gratis/vista-frontal-gafas-sol-arena-playa_23-2148557643.jpg?size=626&ext=jpg&ga=GA1.1.649413161.1697839706&semt=ais" class="rounded-m shadow-xl" width="80">
+                </div>
+                <div class="ms-3">
+                    <h5 class="font-600 opacity-50">Lentes de sol</h5>
+                    <h4 class="pt-1 color-highlight">$4<sup>99</sup></h4>
+                    <span class="text-muted">(4 dias)</span>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
