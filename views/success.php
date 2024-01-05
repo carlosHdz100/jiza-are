1a17030da7e1a04d38d92fabe9d68e9f886f3a9e
Daniel <alvarezdanie0@gmail.com>
2024-01-04 18:58:38 -0600

    add menssage

--- views/success.php
+++ views/success.php
@@ -1,4 +1,19 @@
 <div class="page-content mt-2">
 
+    <div class="container-fluid bg-success d-flex flex-column  align-items-center justify-content-center" style="min-height: 400px;">
+        <h1 class="color-white">Felicidades Daniel</h1>
+        <h2 class="color-white">Tu renta ha sido exitosa</h2>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-discount-check" width="76" height="76" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7c.412 .41 .97 .64 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1c0 .58 .23 1.138 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55v-1" />
+            <path d="M9 12l2 2l4 -4" />
+        </svg>
+    </div>
+
+    <div class="d-flex justify-content-center mt-5">
+        <button type="button" class="btn btn-dark me-2 p-3  ">Ver mi renta</button>
+
+        <button type="button" class="btn btn-dark p-3">Ir al inicio</button>
+    </div>
 
 </div>
